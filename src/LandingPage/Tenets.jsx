f48c8178cb0b1a0384433cba5c7d0b7b90cac4a2
JJ <abrassiv3@gmail.com>
2025-01-07 11:56:01 +0300

    tenets

--- src/LandingPage/Tenets.jsx
+++ src/LandingPage/Tenets.jsx
@@ -12,18 +12,18 @@ function Tenets() {
         <div className="tenets">
             <div className="tenets-top">
                 <div className="tenets-top-title">
-                <h2>[Tenets]</h2>
+                    <h2>[Tenets]</h2>
                 </div>
                 <div className="tenets-top-list">
-    {tenets.map((tenet) => (
-        <div key={tenet.id} className="tenets-list-item">
-            <span className="tenets-list-title">{tenet.title}</span>
-            <span className="tenets-list-number">{tenet.id}</span>
-        </div>
-    ))}
-</div>
-
+                    {tenets.map((tenet) => (
+                    <div key={tenet.id} className="tenets-list-item">
+                        <span className="tenets-list-title">{tenet.title}</span>
+                        <span className="tenets-list-number">{tenet.id}</span>
+                    </div>
+                    ))}
+                </div>
             </div>
+
             <div className="tenets-bottom">
                 <div className="tenets-bottom-cards">
                     {tenets.map((tenet) => (
