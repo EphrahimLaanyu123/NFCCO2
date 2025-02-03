2b8d4fabd62a73f7018c21373f17e995b9287fd9
EphrahimLaanyu <elaanyu@gmail.com>
2025-02-03 10:58:04 +0300

    Added api-1

---
+++ .history/src/LandingPage/Gallery_20250203105709.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Gallery.css";
+
+
+// Combine local and external images
+const initialImages = [
+  "https://ik.imagekit.io/fcuzgktdi/assets/_KGP6196.JPG?updatedAt=1735723840497",
+  "https://ik.imagekit.io/fcuzgktdi/assets/_KGP6196.JPG?updatedAt=1735723840497",
+  "https://ik.imagekit.io/fcuzgktdi/assets/_KGP6196.JPG?updatedAt=1735723840497",
+  "https://ik.imagekit.io/fcuzgktdi/assets/_KGP6196.JPG?updatedAt=1735723840497",
+  "https://ik.imagekit.io/fcuzgktdi/assets/pexels-joe-arts-1830507-3505000.jpg?updatedAt=1735723666739",
+];
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      <div className="gallery__scroll">
+        {initialImages.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`gallery-img-${index}`}
+            className="gallery__image"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
